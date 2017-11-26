9033916a5ce022de08d716087969334ff6c760bf
ljw26 <ljw26@st-andrews.ac.uk>
2017-11-26 15:59:11 +0000

    Basic implementation of svm with tester method to show functionality

---
+++ SVM/svm/svm.py
@@ -0,0 +1,37 @@
+from sklearn import svm
+from sklearn import datasets
+
+import matplotlib.pyplot as plt
+
+"""Class for operations of Support Vector Machines (SVM's)"""
+class SupportVectorMachine():
+
+    """Create classifier for SVM"""
+    @staticmethod
+    def _make_svm_classifier():
+        return svm.SVC(gamma=0.001, C=100)
+
+    """Build classifier for SVM"""
+    def __init__(self):
+        self.classifier = svm.SVC(gamma=0.001, C=100)
+
+    """Trains the SVM on imported data"""
+    def fit(self, data, features):
+        X, y = data, features
+        return self.classifier.fit(X, y)
+
+    """Predicts value of specified data point"""
+    def predict(self, data_to_predict):
+        return self.classifier.predict(data_to_predict)
+
+    def test(self):
+        digits = datasets.load_digits()
+        self.fit(digits.data[:-10], digits.target[:-10])
+        print("Prediction =", self.predict(digits.data[-2]))
+        plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
+        plt.show()
+
+if __name__ == "__main__":
+    svm = SupportVectorMachine()
+    svm.test()
+
