dc82700b4e7c94f67bffedbfa13d637e55c38681
JKhandelwal <jk218@st-andrews.ac.uk>
2017-12-05 15:18:41 +0000

    temporary commit

--- ServerWrapper.py
+++ ServerWrapper.py
@@ -2,4 +2,12 @@ from flask import request
 
 def getHeaderValue(name):
     ret = request.headers.get(name)
-    if (ret)
+    dataset = requests.headers.get("dataset")
+    if ret = 'NB':
+        return naive_bayes(dataset)
+    elif ret = "RF":
+        return random_forests(dataset)
+    elif ret = "SVM":
+        return support_vector_machines(dataset)
+    elif ret = "KFOLD":
+        return;
