388dafd89f822b76cf99418d3fcc59c517da3500
ljw26 <ljw26@st-andrews.ac.uk>
2018-01-31 16:06:18 +0000

    started implementing statistical analysis tests

--- SVM/svm/svm.py
+++ SVM/svm/svm.py
@@ -8,12 +8,12 @@ class SupportVectorMachine():
 
     """Create classifier for SVM"""
     @staticmethod
-    def _make_svm_classifier():
-        return svm.SVC(gamma=0.001, C=100)
+    def _make_svm_classifier(gamma_value):
+        return svm.SVC(gamma=gamma_value, C=100)
 
     """Build classifier for SVM"""
-    def __init__(self):
-        self.classifier = SupportVectorMachine._make_svm_classifier()
+    def __init__(self, gamma_value):
+        self.classifier = SupportVectorMachine._make_svm_classifier(gamma_value)
 
     """Trains the SVM on imported data"""
     def fit(self, data, features):
@@ -28,10 +28,30 @@ class SupportVectorMachine():
         digits = datasets.load_digits()
         self.fit(digits.data[:-10], digits.target[:-10])
         print("Prediction =", self.predict(digits.data[-2]))
+        print ("Actual =", digits.target[-2])
         plt.imshow(digits.images[-2], cmap=plt.cm.gray_r, interpolation="nearest")
         plt.show()
 
-if __name__ == "__main__":
-    svm = SupportVectorMachine()
-    svm.test()
 
+def gamma_value_tests():
+    print "Analysis of altering SVM gamma value:"
+    print "Gamma value= 1"
+    digits = datasets.load_digits()
+    svm_gamma1 = SupportVectorMachine(0.01)
+    svm_gamma1.fit(digits.data[:-10], digits.target[:-10])
+    accuracy1 = 0
+
+    for i in range(-9, 0):
+        prediction = svm_gamma1.predict(digits.data[i].reshape(1,-1))
+        print "Prediction for ", i, prediction
+        print "Actual= ", digits.target[i]
+        if prediction == digits.target[i]:
+            accuracy1 + 1
+
+    print accuracy1
+
+
+if __name__ == "__main__":
+    # svm = SupportVectorMachine(0.01)
+    # svm.test()
+    gamma_value_tests()
