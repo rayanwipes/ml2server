a5395a7ae50d155494da5c1912d50924efc07331
JKhandelwal <khandelwaljalaj16@gmail.com>
2018-01-31 15:40:04 +0000

    cleaning of the data added

--- wrapper.py
+++ wrapper.py
@@ -3,18 +3,27 @@ import csv
 def getData(request):
     df = pd.DataFrame(request)
     df.dropna(axis=0,how="any")
-    print(df)
+    return df
+    # print(df)
 
+'''
+Differing formats for the wrapper functions
+'''
 def svm(data):
     xData = data['x']
     yData = data['y']
     modeltype =0
-    clf = NaiveBayes(modeltype)
+    clf = SupportVectorMachines(modeltype)
     clf.fit(xData,yData)
     return clf
 
 def rf(data):
-    return
+    xData = data['x']
+    yData = data['y']
+    modeltype =0
+    clf = RandomForests(modeltype)
+    clf.fit(xData,yData)
+    return clf
 
 def nb(data):
     xData = data['x']
@@ -25,23 +34,35 @@ def nb(data):
     return clf
 
 
-def sort(request):
-    data = getData(request)
+OP_TRAIN,OP_FIT,OP_SCORE,OP_PREDICT = range(4)
+MDL_NAIVE_BAYES_GAUSSIAN, MDL_NAIVE_BAYES_MULTINOMIAL,MDL_NAIVE_BAYES_BERNOULLI, MDL_RANDOM_FOREST, MDL_SVM = range(5)
 
-    if request['type'] == "svm":
-        return svm(data)
-    elif request['type'] == "nb":
-        return svm(data)
-    elif request['type'] == "rf":
-        return rf(data)
+def sort(operation,model_type,data_fName,existing_model=None):
+    model = None
+    if model_type = MDL_NAIVE_BAYES_GAUSSIAN:
 
-spamReader = csv.reader(open('publisher.csv', newline=''), delimiter=',', quotechar='|')
+    elif model_type = MDL_NAIVE_BAYES_MULTINOMIAL:
+    elif model_type = MDL_NAIVE_BAYES_BERNOULLI:
+    elif model_type = MDL_NAIVE_BAYES_RANDOM_FOREST:
+    elif model_type = MDL_NAIVE_BAYES_SVM:    
 
-print(spamReader.__next__())
 
-test= {'x': [],'y': []}
-for row in spamReader:
-    test['x'].append(row[0])
-    test['y'].append(row[1])
 
-getData(test)
+    # deal with issues
+    # getting data in the response
+
+    # convert the data format to a dictionary
+    # with the correct headers in place
+
+
+    readers = csv.reader(open('fName.csv', newline=''), delimiter=',', quotechar='|')
+    fName = request['fName']
+    # This was done if there was a header in the file
+    print(readers.__next__())
+
+    dictionary= {'x': [],'y': []}
+    for row in readers:
+        dictionary['x'].append(row[0])
+        dictionary['y'].append(row[1])
+
+    newDF = getData(dictionary)
