7ad24e22e68807299dab6a07be0efb527c877087
JKhandelwal <khandelwaljalaj16@gmail.com>
2018-01-31 10:50:11 +0000

    cleaning of the data added

--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import csv
+def getData(request):
+    df = pd.DataFrame(request)
+    df.dropna(axis=0,how="any")
+    print(df)
+
 def svm(data):
     xData = data['x']
     yData = data['y']
@@ -7,7 +14,7 @@ def svm(data):
     return clf
 
 def rf(data):
-    
+    return
 
 def nb(data):
     xData = data['x']
@@ -19,9 +26,22 @@ def nb(data):
 
 
 def sort(request):
+    data = getData(request)
+
     if request['type'] == "svm":
-        return svm(request['data'])
+        return svm(data)
     elif request['type'] == "nb":
-        return svm(request['nb'])
-    elif request['type'] == "rf"
-        return rf(request['data'])
+        return svm(data)
+    elif request['type'] == "rf":
+        return rf(data)
+
+spamReader = csv.reader(open('publisher.csv', newline=''), delimiter=',', quotechar='|')
+
+print(spamReader.__next__())
+
+test= {'x': [],'y': []}
+for row in spamReader:
+    test['x'].append(row[0])
+    test['y'].append(row[1])
+
+getData(test)
