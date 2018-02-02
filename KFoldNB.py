0625085640639620352eb70cb2af2e34ac2df2bf
JKhandelwal <khandelwaljalaj16@gmail.com>
2018-02-02 14:56:55 +0000

    cleaning up files

--- KFoldNB.py
+++
@@ -1,7 +0,0 @@
-        nb = NaiveBayes(0)
-        n = len(values)
-        average =[]
-        for value in range(0,n):
-            currList = a[:value] + a[value+1:]
-            average.append((value,validate(train(currList),values[i])))
-        totalAverage = float(sum(average)) / float(len(values))
