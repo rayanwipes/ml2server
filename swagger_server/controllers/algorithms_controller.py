baa1c75ab3cf287c3c5396dee6fb5f1961707894
JKhandelwal <jk218@st-andrews.ac.uk>
2018-04-03 22:58:41 +0100

    Incremental commit whilst changing all of the swagger definitions in the algorithms.py file, further changes needed to swaggerify it

--- swagger_server/controllers/algorithms_controller.py
+++ swagger_server/controllers/algorithms_controller.py
@@ -1,16 +1,18 @@
 import connexion
 import six
 from flask import jsonify
+import json
 from swagger_server.models.error import Error  # noqa: E501
 from swagger_server.models.jobs import Jobs  # noqa: E501
 from swagger_server.models.model404_error import Model404Error  # noqa: E501
 from swagger_server.models.request_suggestion import RequestSuggestion  # noqa: E501
 from swagger_server.models.suggest import Suggest  # noqa: E501
 from swagger_server import util
-from swagger_server import feature_support
 
 def jobs():  # noqa: E501
-    return jsonify(feature_support.jobs)
+    supported = json.load(open('./swagger_server/algorithms/feature_support.json'))
+    job = Jobs(supported)
+    return job,200
 
 def suggest(data):  # noqa: E501
     if connexion.request.is_json:
