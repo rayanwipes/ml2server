ec4a395ebda6f3f8344d052b8d8608476ac6ce8f
krb5 <krb5@st-andrews.ac.uk>
2018-04-03 04:17:22 +0100

    csv loader now supports X and y, experimentally

--- Impl/csv_loader.py
+++ Impl/csv_loader.py
@@ -1,13 +1,28 @@
+import csv
 import pandas
 from sklearn.datasets import *
+
+from columnview import *
 
 
 def load_csv(filename, columns=None):
     return pandas.read_csv(filepath_or_buffer=filename, usecols=columns)
 
 
+# def transpose(m):
+#     ll = [[m[x][y] for y in range(len(m))] for x in range(len(m[0]))]
+
+
 def load_csv_xy(filename, ycolumn, xcolumns=None):
-    return [
-        load_csv(xcolumns),  # X
-        load_csv(ycolumn)  # y
-    ]
+    f = open(filename, 'r')
+    no_cols = len(next(csv.reader(f, delimiter=',')))
+    f.close()
+    if xcolumns is None:
+        xcolumns = [i for i in range(no_cols) if i != ycolumn]
+    X = load_csv(filename, xcolumns).T
+    y = load_csv(filename, [ycolumn]).T
+    return [X, y]
+
+
+if __name__ == "__main__":
+    print(load_csv_xy('file.csv', 0))
