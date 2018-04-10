1d3b97255a3f87abe361334c74e9e38092ba89f6
JKhandelwal <jk218@st-andrews.ac.uk>
2018-04-10 16:11:33 +0100

    Fixed non needed code

--- swagger_server/controllers/feature_support.py
+++ swagger_server/controllers/feature_support.py
@@ -17,11 +17,6 @@ Support Vector Machine
 '''
 
 def fetch_list():
-    # nb_g = JobInfo("NaiveBayesGaussian","tabular","MDL_NAIVE_BAYES_GAUSSIAN",False,"Bayesian Algorithm for gaussian distribution",[])
-    # nb_m = JobInfo("NaiveBayesBernoulli","tabular","MDL_NAIVE_BAYES_BERNOULLI",False,"Bayesian Algorithm for Bernoulli distribution",[])
-    # nb_b = JobInfo("NaiveBayesMultiNomial","tabular","MDL_NAIVE_BAYES_MULTINOMIAL",False,"Bayesian Algorithm for MultiNomial distribution",[])
-    # rf =  JobInfo("RandomForests","tabular","MDL_RANDOM_FORESTS",False,"Algorithm which calculates conditional probability",[])
-    # svm = JobInfo("SupportVectorMachine","tabular","MDL_SUPPORT_VECTOR_MACHINE",False,"Algorithm which uses support vectors for classification",[])
     jobsList = []
     jobsList.append(rf)
     jobsList.append(nb_g)
